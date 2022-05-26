79c1ea361c13421bdd7d89adfac36bef87058e31
Brandon Feltz <elloimbrandon@gmail.com>
2022-05-26 10:50:14 -0700

    routes working great and added seed data

--- back-end/models/movie.js
+++ back-end/models/movie.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const movieSchema =  new mongoose.Schema({
-  name: String,
-  release: String,
-  image: String,
-  genre: String, 
-})
+const movieSchema = new mongoose.Schema(
+  {
+    name: String,
+    release: String,
+    image: String,
+    genre: String,
+  },
+  { timestamps: true }
+);
 
-
-const Movies = mongoose.model('Movie', movieSchema)
+const Movies = mongoose.model("Movie", movieSchema);
 //Connection name Movie will show up as movies
 
-module.exports = Movies
+module.exports = Movies;
---
+++ back-end/models/seedMovies.js
@@ -0,0 +1,36 @@
+module.exports = [
+  {
+    name: "The Goonies",
+    release: "1985",
+    image:
+      "https://images.moviesanywhere.com/dfa6105a63a94533c0daba948648c6be/f9be2d29-6e8d-458f-b4d7-9ae6f938e2f2.jpg",
+    genre: "Adventure",
+  },
+  {
+    name: "Sound of Music",
+    release: "1965",
+    image: "https://flxt.tmsimg.com/assets/p5620_p_v8_ao.jpg",
+    genre: "Musical",
+  },
+  {
+    name: "Step Brothers",
+    release: "2008",
+    image:
+      "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/450688_StepBrothers_2008_1400x2100_US.jpg?itok=qfBBSVlL",
+    genre: "Comedy",
+  },
+  {
+    name: "Nacho Libre",
+    release: "2006",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BN2ZkNDgxMjMtZmRiYS00MzFkLTk5ZjgtZDJkZWMzYmUxYjg4XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_FMjpg_UX1000_.jpg",
+    genre: "Comedy",
+  },
+];
+
+//  template
+
+//  name: String,
+//  release: String,
+//  image: String,
+//  genre: String,
